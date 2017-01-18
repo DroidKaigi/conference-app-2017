131a6b8c85e181853494a7644980e2d0af23dc3f
João Orui <joao@joao.jp>
2017-01-18 19:10:00 +0900

    Scripts to upload/download strings from OneSky

---
+++ oneskyapp_upload.rb
@@ -0,0 +1,14 @@
+require 'onesky'
+
+# Create client
+client = Onesky::Client.new('4SE1TdvPn48nctD9UmRnOMnacTjVHF2C', ENV['ONE_SKY_SECRET_KEY'])
+
+# show project details
+project_id = 94974
+project = client.project(project_id)
+resp = JSON.parse(project.show)
+p resp['data']
+
+# upload file
+resp = project.upload_file(file: 'strings.xml', file_format: 'ANDROID_XML', is_keeping_all_strings: 'false')
+p resp.code # => 202
