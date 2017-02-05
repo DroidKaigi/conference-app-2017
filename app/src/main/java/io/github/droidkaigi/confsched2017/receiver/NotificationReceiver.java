ae622a4535c463c32b8e59ba4fc6359db4055b00
Keisuke Kobayashi <k.kobayashi.122@gmail.com>
2017-02-06 02:17:44 +0900

    Register alarm to show notification

---
+++ app/src/main/java/io/github/droidkaigi/confsched2017/receiver/NotificationReceiver.java
@@ -0,0 +1,49 @@
+package io.github.droidkaigi.confsched2017.receiver;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.content.ContextCompat;
+
+import io.github.droidkaigi.confsched2017.R;
+import io.github.droidkaigi.confsched2017.view.activity.SessionDetailActivity;
+
+public class NotificationReceiver extends BroadcastReceiver {
+
+    public static final String KEY_SESSION_ID = "session_id";
+    public static final String KEY_TITLE = "title";
+    public static final String KEY_TEXT = "text";
+
+    private static final int NOTIFICATION_ID = 1;
+
+    public static Intent createIntent(Context context, int sessionId, String title, String text) {
+        Intent intent = new Intent(context, NotificationReceiver.class);
+        intent.putExtra(NotificationReceiver.KEY_SESSION_ID, sessionId);
+        intent.putExtra(NotificationReceiver.KEY_TITLE, title);
+        intent.putExtra(NotificationReceiver.KEY_TEXT, text);
+        return intent;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        int sessionId = intent.getIntExtra(KEY_SESSION_ID, 0);
+        String title = intent.getStringExtra(KEY_TITLE);
+        String text = intent.getStringExtra(KEY_TEXT);
+        Intent intent1 = SessionDetailActivity.createIntent(context, sessionId);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent1, 0);
+        Notification notification = new NotificationCompat.Builder(context)
+                .setTicker(title)
+                .setContentTitle(title)
+                .setContentText(text)
+                .setSmallIcon(R.mipmap.ic_launcher) // TODO Please replace this icon
+                .setColor(ContextCompat.getColor(context, R.color.theme))
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+                .build();
+        NotificationManagerCompat.from(context).notify(NOTIFICATION_ID, notification);
+    }
+}
--- app/src/main/java/io/github/droidkaigi/confsched2017/viewmodel/SessionDetailViewModel.java
+++ app/src/main/java/io/github/droidkaigi/confsched2017/viewmodel/SessionDetailViewModel.java
@@ -1,6 +1,9 @@
 package io.github.droidkaigi.confsched2017.viewmodel;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.databinding.BaseObservable;
 import android.support.annotation.ColorRes;
 import android.support.annotation.NonNull;
@@ -18,6 +21,7 @@ import javax.inject.Inject;
 import io.github.droidkaigi.confsched2017.R;
 import io.github.droidkaigi.confsched2017.model.Session;
 import io.github.droidkaigi.confsched2017.model.TopicColor;
+import io.github.droidkaigi.confsched2017.receiver.NotificationReceiver;
 import io.github.droidkaigi.confsched2017.repository.sessions.MySessionsRepository;
 import io.github.droidkaigi.confsched2017.repository.sessions.SessionsRepository;
 import io.github.droidkaigi.confsched2017.util.DateUtil;
@@ -139,10 +143,12 @@ public class SessionDetailViewModel extends BaseObservable implements ViewModel 
             mySessionsRepository.delete(session)
                     .subscribe((result) -> Log.d(TAG, "Deleted my session"),
                             throwable -> Log.e(TAG, "Failed to delete my session", throwable));
+            unregisterAlarm();
         } else {
             mySessionsRepository.save(session)
                     .subscribe(() -> Log.d(TAG, "Saved my session"),
                             throwable -> Log.e(TAG, "Failed to save my session", throwable));
+            registerAlarm();
         }
 
         if (callback != null) {
@@ -158,6 +164,35 @@ public class SessionDetailViewModel extends BaseObservable implements ViewModel 
                 DateUtil.getLongFormatDate(displaySTime),
                 DateUtil.getHourMinute(displayETime),
                 DateUtil.getMinutes(displaySTime, displayETime));
+    }
+
+    private PendingIntent createAlarmIntent() {
+        int REQ_CODE_NOTIFICATION = 1001;
+        String title = context.getString(R.string.notitication_title, this.session.title);
+        Date displaySTime = LocaleUtil.getDisplayDate(session.stime, context);
+        Date displayETime = LocaleUtil.getDisplayDate(session.etime, context);
+        String room = "";
+        if (this.session.room != null) {
+            room = this.session.room.name;
+        }
+        String text = context.getString(R.string.notification_message,
+                DateUtil.getHourMinute(displaySTime),
+                DateUtil.getHourMinute(displayETime),
+                room);
+        Intent intent = NotificationReceiver.createIntent(context, this.session.id, title, text);
+        return PendingIntent.getBroadcast(context, REQ_CODE_NOTIFICATION,
+                intent, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+    private void registerAlarm() {
+        long time = System.currentTimeMillis() + 5000L; // TODO 5 sec after for development
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, time, createAlarmIntent());
+    }
+
+    private void unregisterAlarm() {
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        alarmManager.cancel(createAlarmIntent());
     }
 
     public String getSessionTitle() {
