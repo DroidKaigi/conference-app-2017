afa02738313df1650fd5034d879d0b718306f025
futabooo <mail@futabooo.com>
2017-02-07 02:35:42 +0900

    Add Session Feedback Button

--- app/src/main/java/io/github/droidkaigi/confsched2017/api/DroidKaigiClient.java
+++ app/src/main/java/io/github/droidkaigi/confsched2017/api/DroidKaigiClient.java
@@ -9,9 +9,12 @@ import javax.inject.Singleton;
 
 import io.github.droidkaigi.confsched2017.api.service.DroidKaigiService;
 import io.github.droidkaigi.confsched2017.api.service.GithubService;
+import io.github.droidkaigi.confsched2017.api.service.GoogleFormService;
 import io.github.droidkaigi.confsched2017.model.Contributor;
 import io.github.droidkaigi.confsched2017.model.Session;
+import io.github.droidkaigi.confsched2017.model.SessionFeedback;
 import io.reactivex.Single;
+import retrofit2.Response;
 
 @Singleton
 public class DroidKaigiClient {
@@ -20,14 +23,17 @@ public class DroidKaigiClient {
 
     private final GithubService githubService;
 
+    private final GoogleFormService googleFormService;
+
     private static final int INCLUDE_ANONYMOUS = 1;
 
     private static final int MAX_PER_PAGE = 100;
 
     @Inject
-    public DroidKaigiClient(DroidKaigiService droidKaigiService, GithubService githubService) {
+    public DroidKaigiClient(DroidKaigiService droidKaigiService, GithubService githubService, GoogleFormService googleFormService) {
         this.droidKaigiService = droidKaigiService;
         this.githubService = githubService;
+        this.googleFormService = googleFormService;
     }
 
     public Single<List<Session>> getSessions(@NonNull String languageId) {
@@ -40,5 +46,9 @@ public class DroidKaigiClient {
 
     public Single<List<Contributor>> getContributors() {
         return githubService.getContributors("DroidKaigi", "conference-app-2017", INCLUDE_ANONYMOUS, MAX_PER_PAGE);
+    }
+
+    public Single<Response<Void>>submitSessionFeedback(SessionFeedback sessionFeedback){
+        return googleFormService.submitSessionFeedback(sessionFeedback.sessionId);
     }
 }
