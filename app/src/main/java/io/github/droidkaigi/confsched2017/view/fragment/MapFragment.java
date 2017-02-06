b3570636c627a6a7b96dc9de603634d38caf7c20
amay077 <okuokuoku@gmail.com>
2017-02-06 20:46:38 +0900

    Add mapview in MapFragment

--- app/src/main/java/io/github/droidkaigi/confsched2017/view/fragment/MapFragment.java
+++ app/src/main/java/io/github/droidkaigi/confsched2017/view/fragment/MapFragment.java
@@ -7,6 +7,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+import io.github.droidkaigi.confsched2017.R;
 import io.github.droidkaigi.confsched2017.databinding.FragmentMapBinding;
 
 public class MapFragment extends BaseFragment {
@@ -30,11 +37,20 @@ public class MapFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentMapBinding.inflate(inflater, container, false);
 
+        // TODO Is possible get from FragmentMapBinding?
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                // TODO Move to MapViewModel
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(35.6961555, 139.6910507), 19f)); // bellesalle shinjuku
+            }
+        });
+
         initView();
         return binding.getRoot();
     }
 
     private void initView() {
-        //
     }
 }
