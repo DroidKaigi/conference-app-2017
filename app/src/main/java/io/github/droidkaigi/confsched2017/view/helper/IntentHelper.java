a06070d8878e51ba9a8d0b795842630cae541f99
Naoya Shibahara <naoya@cps.im.dendai.ac.jp>
2017-02-07 16:13:14 +0900

    Add javadoc

--- app/src/main/java/io/github/droidkaigi/confsched2017/view/helper/IntentHelper.java
+++ app/src/main/java/io/github/droidkaigi/confsched2017/view/helper/IntentHelper.java
@@ -9,6 +9,10 @@ import timber.log.Timber;
 
 public class IntentHelper {
 
+    /**
+     * Builds an intent of type Intent.ACTION_VIEW from the passed htmlUrl.
+     * But, When intent.resolveActivity(context.getPackageManager()) == null is true, it return null.
+     */
     public static Intent buildActionViewIntent(@NonNull Context context, @NonNull String htmlUrl) {
         Timber.i("buildActionViewIntent: url: %s", htmlUrl);
 
