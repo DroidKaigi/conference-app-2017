d076efd5d163132c4224f50a57479d9d3b477927
Keita Watanabe <Nal3eCh@gmail.com>
2017-02-07 20:12:08 +0900

    Divide sdk version
    Divide sdk version on Jerry Bean MR1 and MR2 because using them in DateUtil.

--- app/src/test/java/io/github/droidkaigi/confsched2017/util/DateUtilTest.kt
+++ app/src/test/java/io/github/droidkaigi/confsched2017/util/DateUtilTest.kt
@@ -1,11 +1,13 @@
 package io.github.droidkaigi.confsched2017.util
 
+import android.os.Build
 import org.junit.Assert.assertNotNull
 import org.junit.Assert.fail
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.RuntimeEnvironment
+import org.robolectric.annotation.Config
 import java.lang.reflect.InvocationTargetException
 import java.util.*
 
@@ -13,6 +15,7 @@ import java.util.*
  * Created by KeishinYokomaku on 2017/01/17.
  */
 @RunWith(RobolectricTestRunner::class)
+@Config(sdk = intArrayOf(Build.VERSION_CODES.JELLY_BEAN_MR1, Build.VERSION_CODES.JELLY_BEAN_MR2))
 class DateUtilTest {
     @Test
     @Throws(Exception::class)
