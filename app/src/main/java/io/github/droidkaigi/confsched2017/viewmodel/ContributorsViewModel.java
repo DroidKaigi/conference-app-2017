bf416667e68712d0fdb0c5e6836ef4ddb37cd32d
t-kashima <takumi.kashima@nanapi.co.jp>
2017-02-07 23:10:06 +0900

    Add loading to contributors page

--- app/src/main/java/io/github/droidkaigi/confsched2017/viewmodel/ContributorsViewModel.java
+++ app/src/main/java/io/github/droidkaigi/confsched2017/viewmodel/ContributorsViewModel.java
@@ -1,22 +1,29 @@
 package io.github.droidkaigi.confsched2017.viewmodel;
 
-import com.annimon.stream.Collectors;
-import com.annimon.stream.Stream;
-
 import android.databinding.BaseObservable;
+import android.databinding.Bindable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.View;
+
+import com.annimon.stream.Collectors;
+import com.annimon.stream.Stream;
 
 import java.util.List;
 
 import javax.inject.Inject;
 
+import io.github.droidkaigi.confsched2017.BR;
+import io.github.droidkaigi.confsched2017.model.Contributor;
 import io.github.droidkaigi.confsched2017.repository.contributors.ContributorsRepository;
 import io.reactivex.Single;
 
 public final class ContributorsViewModel extends BaseObservable implements ViewModel, ContributorViewModel.Callback {
 
     private final ContributorsRepository contributorsRepository;
+
+    @Nullable
+    private List<Contributor> contributors;
 
     @Nullable
     private Callback callback;
@@ -35,18 +42,31 @@ public final class ContributorsViewModel extends BaseObservable implements ViewM
     }
 
     public Single<List<ContributorViewModel>> getContributors() {
-        return contributorsRepository.findAll().map(contributors ->
-                Stream.of(contributors).map(contributor -> {
-                    ContributorViewModel viewModel = new ContributorViewModel(contributor);
-                    viewModel.setCallback(this);
-                    return viewModel;
-                }).collect(Collectors.toList()));
+        return contributorsRepository.findAll().map(contributors -> {
+            this.contributors = contributors;
+            notifyPropertyChanged(BR.loadingVisibility);
+
+            return Stream.of(contributors).map(contributor -> {
+                ContributorViewModel viewModel = new ContributorViewModel(contributor);
+                viewModel.setCallback(this);
+                return viewModel;
+            }).collect(Collectors.toList());
+        });
     }
 
     @Override
     public void onClickContributor(String htmlUrl) {
         if (callback != null) {
             callback.onClickContributor(htmlUrl);
+        }
+    }
+
+    @Bindable
+    public int getLoadingVisibility() {
+        if (this.contributors == null) {
+            return View.VISIBLE;
+        } else {
+            return View.GONE;
         }
     }
 
