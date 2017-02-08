6c9b6b854f7e7130e0e112be4c5b0f7f229efc24
tomoima525 <tomoima525@gmail.com>
2017-02-08 10:54:56 -0800

    Add inject annotation for release build

--- app/src/release/java/io/github/droidkaigi/confsched2017/debug/ClearCache.java
+++ app/src/release/java/io/github/droidkaigi/confsched2017/debug/ClearCache.java
@@ -5,8 +5,14 @@ import com.tomoima.debot.strategy.DebotStrategy;
 import android.app.Activity;
 import android.support.annotation.NonNull;
 
+import javax.inject.Inject;
+
 
 public class ClearCache extends DebotStrategy {
+
+    @Inject
+    public ClearCache() {
+    }
 
     @Override
     public void startAction(@NonNull Activity activity) {
