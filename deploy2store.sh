657be4630dc8940ff98511d3c9370abd64dd35c9
Ryo Sakaguchi <rsakaguchi3125@gmail.com>
2017-02-08 23:45:14 +0900

    Write deploy shell to GooglePlay

---
+++ deploy2store.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+fastlane supply run -p io.github.droidkaigi.confsched2017 \
+-a alpha \
+-j [RELEASE_JSON_KEY_FILE_PATH]] \
+-u [APK_FILE_PATH] \
+--verbose --skip_upload_images --skip_upload_screenshots
