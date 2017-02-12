e9116c0c95a43d6c387ad4c0bbfeebfb9aaa9d16
FUJI Goro (gfx) <gfuji@cpan.org>
2017-02-12 12:50:58 +0900

    use Relation#upsert() with Orma 4.2.0-rc2

--- app/src/main/java/io/github/droidkaigi/confsched2017/repository/sessions/SessionsLocalDataSource.java
+++ app/src/main/java/io/github/droidkaigi/confsched2017/repository/sessions/SessionsLocalDataSource.java
@@ -1,23 +1,15 @@
 package io.github.droidkaigi.confsched2017.repository.sessions;
 
-import com.github.gfx.android.orma.annotation.OnConflict;
-
-import android.database.sqlite.SQLiteConstraintException;
-
 import java.util.List;
 import java.util.Locale;
 
 import javax.inject.Inject;
 
 import io.github.droidkaigi.confsched2017.model.OrmaDatabase;
-import io.github.droidkaigi.confsched2017.model.Room;
 import io.github.droidkaigi.confsched2017.model.Room_Relation;
 import io.github.droidkaigi.confsched2017.model.Session;
 import io.github.droidkaigi.confsched2017.model.Session_Relation;
-import io.github.droidkaigi.confsched2017.model.Session_Updater;
-import io.github.droidkaigi.confsched2017.model.Speaker;
 import io.github.droidkaigi.confsched2017.model.Speaker_Relation;
-import io.github.droidkaigi.confsched2017.model.Topic;
 import io.github.droidkaigi.confsched2017.model.Topic_Relation;
 import io.reactivex.Maybe;
 import io.reactivex.Single;
@@ -67,59 +59,11 @@ public final class SessionsLocalDataSource implements SessionsDataSource {
 
     }
 
-    private void upsertSpeaker(Speaker speaker) {
-        if (speaker != null) {
-            speakerRelation().upsert(speaker);
-        }
-    }
-
-    private void upsertRoom(Room room) {
-        if (room != null) {
-            placeRelation().upsert(room);
-        }
-    }
-
-    private void upsertCategory(Topic topic) {
-        if (topic != null) {
-            topicRelation().upsert(topic);
-        }
-    }
-
     private void updateAllSync(List<Session> sessions) {
-        for (Session session : sessions) {
-            upsertSpeaker(session.speaker);
-            upsertCategory(session.topic);
-            upsertRoom(session.room);
+        Session_Relation relation = sessionRelation();
 
-            try {
-                sessionRelation().inserter(OnConflict.FAIL).execute(session);
-            } catch (SQLiteConstraintException e) {
-                // failed by conflict, so try update one.
-                Session_Updater sessionUpdater = sessionRelation().updater()
-                        .id(session.id)
-                        .title(session.title)
-                        .desc(session.desc)
-                        .stime(session.stime)
-                        .etime(session.etime)
-                        .durationMin(session.durationMin)
-                        .type(session.type)
-                        .lang(session.lang)
-                        .slideUrl(session.slideUrl)
-                        .movieUrl(session.movieUrl)
-                        .movieDashUrl(session.movieDashUrl)
-                        .shareUrl(session.shareUrl);
-                // nullable association fields
-                if (session.speaker != null) {
-                    sessionUpdater.speaker(session.speaker);
-                }
-                if (session.topic != null) {
-                    sessionUpdater.topic(session.topic);
-                }
-                if (session.room != null) {
-                    sessionUpdater.room(session.room);
-                }
-                sessionUpdater.idEq(session.id).execute();
-            }
+        for (Session session : sessions) {
+            relation.upsert(session);
         }
     }
 
