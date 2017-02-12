1749fe077f151e6d2923f56a0797e1ba5c7efe68
Keita Watanabe <Nal3eCh@gmail.com>
2017-02-12 09:14:48 +0900

    Add succeed and fail tests

--- app/src/test/java/io/github/droidkaigi/confsched2017/util/AssetsUtilTest.kt
+++ app/src/test/java/io/github/droidkaigi/confsched2017/util/AssetsUtilTest.kt
@@ -1,9 +1,12 @@
 package io.github.droidkaigi.confsched2017.util
 
+import com.google.firebase.FirebaseApp
+import com.taroid.knit.should
 import org.junit.Assert.fail
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
 import java.lang.reflect.InvocationTargetException
 
 @RunWith(RobolectricTestRunner::class)
@@ -19,5 +22,26 @@ class AssetsUtilTest {
             if (e.cause !is AssertionError)
                 fail()
         }
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun loadJSONFromAsset_succseedsWhenFileExists() {
+        val context = RuntimeEnvironment.application
+        context.assets.list("json").forEach {
+            val expect = context.assets.open("json/" + it)
+                                       .reader(charset = Charsets.UTF_8)
+                                       .use { it.readText() }
+            val actual = AssetsUtil.loadJSONFromAsset(context, it)
+
+            actual.should be expect
+        }
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun loadJSONFromAsset_failsWhenFileNotExists() {
+        FirebaseApp.initializeApp(RuntimeEnvironment.application)
+        AssetsUtil.loadJSONFromAsset(RuntimeEnvironment.application, "NonExistsFile.json").should be null
     }
 }
