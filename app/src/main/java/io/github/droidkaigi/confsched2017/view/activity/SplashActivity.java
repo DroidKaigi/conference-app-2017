4d8c2ae970b079e3f755a5d38aa2e574be2b3b5e
Kaoru Tsutsumishita <roana.enter@gmail.com>
2017-02-12 23:48:10 +0900

    Fix Runnabe stop if press back button

--- app/src/main/java/io/github/droidkaigi/confsched2017/view/activity/SplashActivity.java
+++ app/src/main/java/io/github/droidkaigi/confsched2017/view/activity/SplashActivity.java
@@ -20,6 +20,12 @@ public class SplashActivity extends AppCompatActivity {
     private static final String TAG = SplashActivity.class.getSimpleName();
 
     private ActivitySplashBinding binding;
+    private final ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+    private final Handler handler = new Handler();
+    private final Runnable moveActivityRunnable = () -> {
+        scheduledExecutorService.shutdown();
+        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,18 +37,19 @@ public class SplashActivity extends AppCompatActivity {
                     View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
         }
 
-        final ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
         scheduledExecutorService.scheduleAtFixedRate(
                 () -> binding.particleAnimationView.postInvalidate(),
                 0L,
                 40L,
                 TimeUnit.MILLISECONDS);
 
-        new Handler().postDelayed(() -> {
-            scheduledExecutorService.shutdown();
-            startActivity(new Intent(SplashActivity.this, MainActivity.class));
-        }, 3000);
+        handler.postDelayed(moveActivityRunnable, 3000);
+    }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        handler.removeCallbacks(moveActivityRunnable);
     }
 
 }
