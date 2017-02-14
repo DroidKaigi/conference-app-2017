6f190af8448a853670d14ce6e4a8155354770eab
Ryo Sakaguchi <rsakaguchi3125@gmail.com>
2017-02-15 01:23:27 +0900

    Untrack deploy shell

--- deploy2store.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-fastlane supply run -p io.github.droidkaigi.confsched2017 \
--a alpha \
--j ./key.json \
--u /app/build/outputs/apk/[YOUR_APK_FILE_NAME] \
---verbose --skip_upload_images --skip_upload_screenshots
