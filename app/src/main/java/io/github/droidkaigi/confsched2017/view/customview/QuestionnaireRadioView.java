4ab8dad35064bc04b6d78cfb3f7a164289941e80
Gen <archiherewego@gmail.com>
2017-03-04 15:35:58 +0900

    Implement other option in QuestionnaireRadioView

--- app/src/main/java/io/github/droidkaigi/confsched2017/view/customview/QuestionnaireRadioView.java
+++ app/src/main/java/io/github/droidkaigi/confsched2017/view/customview/QuestionnaireRadioView.java
@@ -2,8 +2,11 @@ package io.github.droidkaigi.confsched2017.view.customview;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
-import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
@@ -14,8 +17,13 @@ import io.github.droidkaigi.confsched2017.R;
  */
 
 public class QuestionnaireRadioView extends QuestionnaireView {
+
     private RadioGroup radioGroup;
+
+    private EditText otherValueEditText;
+
     private String[] labels;
+
     private String[] values;
 
     public QuestionnaireRadioView(Context context) {
@@ -34,6 +42,11 @@ public class QuestionnaireRadioView extends QuestionnaireView {
             return;
         }
 
+        parseAttributes(context, attrs);
+        initQuestionnaireContainer(context);
+    }
+
+    private void parseAttributes(Context context, AttributeSet attrs) {
         TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.QuestionnaireRadioView);
         int labelId = a.getResourceId(R.styleable.QuestionnaireRadioView_radioItemsKey, -1);
         int valueId = a.getResourceId(R.styleable.QuestionnaireRadioView_radioItemsValue, -1);
@@ -44,16 +57,75 @@ public class QuestionnaireRadioView extends QuestionnaireView {
         if (labels.length != values.length) {
             throw new RuntimeException("Keys size and values size are not equal.");
         }
+    }
 
-        radioGroup = new RadioGroup(context, attrs);
+    private void initQuestionnaireContainer(Context context) {
+        radioGroup = new RadioGroup(context);
+        LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
         radioGroup.setOrientation(RadioGroup.VERTICAL);
+        radioGroup.setLayoutParams(params);
+
         for (int i = 0; i < labels.length; i++) {
-            RadioButton button = new RadioButton(context, attrs);
-            button.setLayoutParams(new RadioGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+            RadioButton button = new RadioButton(context);
+            button.setLayoutParams(params);
             button.setText(labels[i]);
             radioGroup.addView(button);
         }
-        radioGroup.setOnCheckedChangeListener((group, checkedId) -> setValue(values[checkedId - 1]));
+        if (hasItemOther) {
+            addOtherField(context);
+        }
+        radioGroup.setOnCheckedChangeListener((group, checkedId) -> {
+            if (checkedId > values.length) {
+                // select other field
+                selectOtherField(true);
+                setValue(getResources().getString(QuestionnaireView.OTHER_POST_VALUE_RES_ID));
+            } else {
+                setValue(values[checkedId - 1]);
+                setOtherValue("");
+                selectOtherField(false);
+            }
+        });
         binding.questionnaireContainer.addView(radioGroup);
+    }
+
+    private void addOtherField(Context context) {
+        LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+        RadioButton button = new RadioButton(context);
+        button.setLayoutParams(params);
+        button.setText(getResources().getString(R.string.questionnaire_other_field_label));
+        radioGroup.addView(button);
+
+        otherValueEditText = new EditText(context);
+        otherValueEditText.setLayoutParams(params);
+        otherValueEditText.setInputType(EditorInfo.TYPE_CLASS_TEXT);
+        otherValueEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                setOtherValue(s.toString());
+            }
+        });
+        otherValueEditText.setOnFocusChangeListener((v, hasFocus) -> {
+            if (hasFocus) {
+                radioGroup.check(labels.length + 1);
+            }
+        });
+        radioGroup.addView(otherValueEditText);
+    }
+
+    private void selectOtherField(boolean isSelected) {
+        if (isSelected) {
+            otherValueEditText.setHint(R.string.questionnaire_other_field_hint);
+        } else {
+            otherValueEditText.setHint("");
+            otherValueEditText.setText("");
+        }
     }
 }
--- app/src/main/java/io/github/droidkaigi/confsched2017/view/customview/QuestionnaireView.java
+++ app/src/main/java/io/github/droidkaigi/confsched2017/view/customview/QuestionnaireView.java
@@ -7,6 +7,7 @@ import android.databinding.InverseBindingAdapter;
 import android.databinding.InverseBindingListener;
 import android.databinding.InverseBindingMethod;
 import android.databinding.InverseBindingMethods;
+import android.support.annotation.StringRes;
 import android.support.v7.widget.CardView;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
@@ -18,15 +19,21 @@ import io.github.droidkaigi.confsched2017.databinding.ViewQuestionnaireBinding;
  * Copyright 2017 G-CREATE
  */
 
-@InverseBindingMethods(
+@InverseBindingMethods({
         @InverseBindingMethod(
                 type = QuestionnaireView.class,
                 attribute = "questionnaireValue",
                 method = "getQuestionnaireValue"
+        ),
+        @InverseBindingMethod(
+                type = QuestionnaireView.class,
+                attribute = "questionnaireOtherValue",
+                method = "getQuestionnaireOtherValue"
         )
-)
+})
 public class QuestionnaireView extends CardView {
-
+    @StringRes
+    protected static final int OTHER_POST_VALUE_RES_ID = R.string.questionnaire_other_field_post_value;
     protected ViewQuestionnaireBinding binding;
     protected String title;
     protected String description;
@@ -34,6 +41,7 @@ public class QuestionnaireView extends CardView {
     protected String otherValue;
     protected boolean hasItemOther;
     protected OnQuestionnaireValueChangeListener listener;
+    protected OnQuestionnaireOtherValueChangedListener otherListener;
 
     public QuestionnaireView(Context context) {
         this(context, null);
@@ -74,11 +82,14 @@ public class QuestionnaireView extends CardView {
     }
 
     public String getOtherValue() {
-        return otherValue != null ? value : "";
+        return otherValue != null ? otherValue : "";
     }
 
     public void setOtherValue(String otherValue) {
         this.otherValue = otherValue;
+        if (otherListener != null) {
+            otherListener.onQuestionnaireOtherValueChanged();
+        }
     }
 
     @InverseBindingAdapter(attribute = "questionnaireValue")
@@ -102,12 +113,40 @@ public class QuestionnaireView extends CardView {
         }
     }
 
+    @InverseBindingAdapter(attribute = "questionnaireOtherValue")
+    public static String getQuestionnaireOtherValue(QuestionnaireView view) {
+        return view.getOtherValue();
+    }
+
+    @BindingAdapter("questionnaireOtherValueAttrChanged")
+    public static void setQuestionnaireOtherValueAttrChanged(QuestionnaireView view, InverseBindingListener listener) {
+        if (listener == null) {
+            view.setListener(null);
+        } else {
+            view.setOtherListener(listener::onChange);
+        }
+    }
+
+    @BindingAdapter("questionnaireOtherValue")
+    public static void setQuestionnaireOtherValue(QuestionnaireView view, String otherValue) {
+        if (!(view.getOtherValue().equals(otherValue))) {
+            view.setOtherValue(otherValue);
+        }
+    }
+
     public void setListener(OnQuestionnaireValueChangeListener listener) {
         this.listener = listener;
     }
 
-    public interface OnQuestionnaireValueChangeListener {
+    public void setOtherListener(OnQuestionnaireOtherValueChangedListener otherListener) {
+        this.otherListener = otherListener;
+    }
 
+    public interface OnQuestionnaireValueChangeListener {
         void onQuestionnaireValueChange(QuestionnaireView view, String currentValue);
+    }
+
+    public interface OnQuestionnaireOtherValueChangedListener {
+        void onQuestionnaireOtherValueChanged();
     }
 }
